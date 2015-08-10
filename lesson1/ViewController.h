e68d8a8c86fc863c95bd669278a9101f1bc950da
Даниил Шайхинуров <daniilsajhinurov@MacBook-Air-Daniil.local>
2015-08-10 13:13:21 +0300

    Initial Commit

---
+++ lesson1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lesson1
+//
+//  Created by Даниил Шайхинуров on 10.08.15.
+//  Copyright (c) 2015 Eleonora Drykina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lesson1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lesson1
+//
+//  Created by Даниил Шайхинуров on 10.08.15.
+//  Copyright (c) 2015 Eleonora Drykina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
